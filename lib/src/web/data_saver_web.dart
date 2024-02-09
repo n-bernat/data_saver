93c0c0bf93bcfa5d35047997c5ac59634991d766
Nikodem Bernat <n.bernat@outlook.com>
2024-02-09 03:18:29 +0100

    Migrate to `package:web` for web support

--- lib/src/web/data_saver_web.dart
+++ lib/src/web/data_saver_web.dart
@@ -1,15 +1,13 @@
-import 'dart:html' as html;
-
 import 'package:data_saver/src/data_saver_platform_interface.dart';
-import 'package:data_saver/src/web/save_data.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:web/web.dart';
 
 /// A web implementation of the DataSaverPlatform of the DataSaver plugin.
 class DataSaverWeb extends DataSaverPlatform {
-  /// Constructs a DataSaverWeb
+  /// Constructs a [DataSaverWeb] instance.
   DataSaverWeb();
 
-  /// Registers a
+  /// Registers a web plugin.
   static void registerWith(Registrar registrar) {
     DataSaverPlatform.instance = DataSaverWeb();
   }
@@ -19,13 +17,15 @@ class DataSaverWeb extends DataSaverPlatform {
   /// See [DataSaverMode] for available options.
   @override
   Future<DataSaverMode> checkMode() async {
-    final connection = html.window.navigator.connection;
-    if (connection == null) {
+    try {
+      return window.navigator.connection.saveData
+          ? DataSaverMode.enabled
+          : DataSaverMode.disabled;
+
+      // If a browser throws an error (doesn't support saveData)
+      // then fallback to DataSaverMode.disabled.
+    } catch (_) {
       return DataSaverMode.disabled;
     }
-
-    return JSConnection.saveData ?? false
-        ? DataSaverMode.enabled
-        : DataSaverMode.disabled;
   }
 }
